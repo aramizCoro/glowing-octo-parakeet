c27e2e82d9a463e2c633d04a3fc53161c72542a7
Emil <emil@aramiz.net>
2018-11-05 13:40:00 +0100

    change title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            New webpage
+            New webpage 2
           </p>
           <a
             className="App-link"
